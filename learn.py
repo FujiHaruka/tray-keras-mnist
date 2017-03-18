03413932400f2dd765f51043faec069c8e341b58
FujiHaruka <fujiharuka@realglobe.jp>
2017-03-18 10:08:29 +0900

    Update

--- learn.py
+++ learn.py
@@ -1,14 +1,14 @@
 from dataset import load_dataset
 from model import create_cnn_model
 
-def learn(batch_size=32, nb_epoch=10):
+def learn(batch_size=32, epochs=10):
     X_train, X_test, Y_train, Y_test, input_shape = load_dataset()
 
     model = create_cnn_model(input_shape)
 
     model.fit(X_train, Y_train,
               batch_size=batch_size,
-              nb_epoch=nb_epoch,
+              epochs=epochs,
               validation_data=(X_test, Y_test))
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -7,10 +7,10 @@ num_classes = 10
 
 def create_cnn_model(input_shape):
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3,
+    model.add(Convolution2D(32, (3,3),
                      activation='relu',
                      input_shape=input_shape))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(64, (3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(BatchNormalization())
